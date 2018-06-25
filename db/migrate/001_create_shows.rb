51ef8d9afe93cd1ae92feec83365a7bcb59b1bc3
Tatyana Scotce <tscotce@gmail.com>
2018-06-25 18:53:30 +0000

    automatically backed up by learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,10 @@
+class Show < ActiveRecord::Migration
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
+end
